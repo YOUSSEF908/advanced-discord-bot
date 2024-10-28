50d81194bef9adafd7edf785080e3a09451b3fbc
AmineDev <76876424+AmineDev07@users.noreply.github.com>
2024-10-28 07:11:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
   token: process.env.token || '',
   mongoURI: process.env.mongoURI || '',
-  clientId: '1299831391596314667',
+  clientId: '',
   clientSecret: process.env.clientSecret || '',
   callbackURL: 'http://localhost:3000/auth/redirect',
   activity: 'Made with ❤️ and discord.js',
