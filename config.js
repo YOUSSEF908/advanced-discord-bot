4124c27a6336e8d77ac8495bc85c818d0cc6ebde
YOUSSEF908 <84159779+YOUSSEF908@users.noreply.github.com>
2025-06-11 20:16:22 +0200

    Create secure environment configuration

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+
 module.exports = {
   token: process.env.token || '',
   mongoURI: process.env.mongoURI || '',
-  clientId: '1322194666447179787',
+  clientId: '1377741387033870377',
   clientSecret: process.env.clientSecret || '',
   callbackURL: 'http://localhost:3000/auth/redirect',
   activity: 'Made with ❤️ and discord.js',
